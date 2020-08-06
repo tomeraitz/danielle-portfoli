f46c4e214edbe67fc8b130c93188d79cb8fd505c
Tomer Raitz <tomeraitz1@gmail.com>
2020-08-06 09:03:45 +0300

    change to json

--- src/redux/actions/formAction.js
+++ src/redux/actions/formAction.js
@@ -7,7 +7,8 @@ export const VAILDTION = 'form : vaildation'
 
 export function sendToMail(mail){
     return  function(dispatch){
-          axios.post(`https://mail-tomer-api.herokuapp.com/index.php/danielleApi` , mail)
+        let dataMail = JSON.stringify(mail)
+          axios.post(`https://mail-tomer-api.herokuapp.com/index.php/danielleApi` , dataMail)
           .then(response => {
               console.log("response.data : ", response.data)
              dispatch({type : MAIL_STATUS ,payload: response.data})
